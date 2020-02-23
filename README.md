03f6cf99ea6a85b622c9b4a9c5b2c7194deab934
Asiqur Rahman <asikur22@gmail.com>
2020-02-23 15:49:29 +0600

    initialization

---
+++ README.md
@@ -0,0 +1,17 @@
+# Wpress Extractor (Mac)
+
+Mac CLI app that allows you to extract .wpress files created by the awesome All-in-one-Wp-Migration Wordpress plugin.
+
+## Credits
+
+The extractor source code : [https://github.com/yani-/wpress](https://github.com/yani-/wpress).
+
+## Download link
+
+[Mac - Download now](https://github.com/Asikur22/wpress/tree/master/dist)
+\*IMPORTANT FOR MAC: Don't forget to make the binary executable by running a `chmod +x wpress` on the downloaded file via the Terminal.
+
+## How to extract/open .wpress files ?
+
+Simply provide a path to your downloaded .wpress file as the first commandline argument to the program.\
+`./wpress /path/to/my/backup.wpress`
---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"github.com/yani-/wpress"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func fileNameWithoutExtension(fileName string) string {
+	return strings.TrimSuffix(fileName, filepath.Ext(fileName))
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	fmt.Printf("Asiq's Wpress Extracter.\n")
+
+	if len(os.Args) == 2 {
+		pathTofile := os.Args[1]
+		fileFolderName := fileNameWithoutExtension(pathTofile)
+		fileName := filepath.Base(pathTofile)
+
+		isFolderCreated := os.Mkdir(fileFolderName, 0755)
+		check(isFolderCreated)
+		fmt.Println(fileFolderName + ", New Folder Created\n")
+
+		newFilePathRaw := filepath.Join(fileFolderName, fileName)
+		newFilePath, newFilePathErr := filepath.Abs(newFilePathRaw)
+		check(newFilePathErr)
+		
+		isRenamed := os.Rename(pathTofile, newFilePath)
+		check(isRenamed)
+		
+		isChdir := os.Chdir(fileFolderName)
+		check(isChdir)
+
+		archiver, _ := wpress.NewReader(newFilePath)
+		_, err := archiver.Extract()
+		if err != nil {
+			fmt.Println("Error = ")
+			fmt.Println(err)
+		} else {
+			fmt.Println("All done!")
+		}
+
+		// fmt.Println("total files = ", i, " files read = ", x);
+	} else {
+		fmt.Println("Inorder to run the extractor please provide the path to the .wpress file as the first argument.")
+	}
+
+	// wpress.Init(archiver);
+
+}
